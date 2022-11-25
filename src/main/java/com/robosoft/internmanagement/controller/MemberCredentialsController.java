6dd541f769141869bc1eaf5aa593a7b4d2c7bab6
PrasadNayak04 <prasad.nayak@robosoftin.com>
2022-11-25 12:07:53 +0530

    Response entity

--- src/main/java/com/robosoft/internmanagement/controller/MemberCredentialsController.java
+++ src/main/java/com/robosoft/internmanagement/controller/MemberCredentialsController.java
@@ -50,7 +50,7 @@ public class MemberCredentialsController {
             final String jwtToken = tokenManager.generateJwtToken(userDetails);
             return ResponseEntity.ok(jwtToken);
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body("false");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("false");
         }
     }
 
