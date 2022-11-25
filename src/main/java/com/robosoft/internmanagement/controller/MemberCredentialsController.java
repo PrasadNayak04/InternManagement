c114b69538d4cef541582345364989ae031c311d
PrasadNayak04 <prasad.nayak@robosoftin.com>
2022-11-25 10:45:09 +0530

    password update bug fi2

--- src/main/java/com/robosoft/internmanagement/controller/MemberCredentialsController.java
+++ src/main/java/com/robosoft/internmanagement/controller/MemberCredentialsController.java
@@ -42,7 +42,7 @@ public class MemberCredentialsController {
         return memberServices.registerMember(memberProfile, request);
     }
 
-    @PostMapping("/login")
+    @PostMapping(value = "/login")
     public ResponseEntity<?> createToken(@RequestBody Member member, HttpServletRequest request) throws Exception {
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(member.getEmailId(), member.getPassword()));
